d41bea638da8843e9bf4a3ea48f082897cb4827d
Joseph Park Dev <human1654@gmail.com>
2024-05-15 22:09:45 +0900

    LicenseUnlockManager 사용 안되는 참조 제거

--- Assets/script/IAPSystem/LicenseUnlockManager.cs
+++ Assets/script/IAPSystem/LicenseUnlockManager.cs
@@ -3,21 +3,6 @@ using UnityEngine;
 public class LicenseUnlockManager : MonoBehaviour
 {
     private bool licenseUnlocked = false;
-    [SerializeField]
-    private GameObject chapterButtonManagerHolder = null;
-    private ChapterButtonManager chapterButtonManager = null;
-
-    private void Awake()
-    {
-        chapterButtonManager = chapterButtonManagerHolder.GetComponent<ChapterButtonManager>();
-        //foreach (Button button in chapterButtonManager.buttons)
-        //{
-        //    Transform lockButtonTransform = button.transform.GetChild(3);
-        //    CodelessIAPButton IAPButton = lockButtonTransform.GetComponent<CodelessIAPButton>();
-        //    IAPButton.onPurchaseComplete
-        //}
-    }
-
     public bool LicensesUnlocked
     {
         get { return licenseUnlocked;}
