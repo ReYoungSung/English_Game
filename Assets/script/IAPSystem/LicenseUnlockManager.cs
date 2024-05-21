bd7fb26d6b7230b6a9c2a5450bf864a96176f438
Joseph Park Dev <human1654@gmail.com>
2024-05-21 15:51:34 +0900

    Google Play Games 연동 테스트 중

--- Assets/script/IAPSystem/LicenseUnlockManager.cs
+++ Assets/script/IAPSystem/LicenseUnlockManager.cs
@@ -1,4 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
 
 public class LicenseUnlockManager : MonoBehaviour
 {
@@ -9,9 +13,50 @@ public class LicenseUnlockManager : MonoBehaviour
         set { licenseUnlocked = value; }
     }
 
+    private void Start()
+    {
+        //var config = new PlayGamesClientConfiguration.Builder().EnableSavedGames().Build();
+        //PlayGamesPlatform.InitializeInstance(config);
+        PlayGamesPlatform.DebugLogEnabled = true;
+        PlayGamesPlatform.Activate();
+    }
+
+    private void SignIn()
+    {
+        PlayGamesPlatform.Instance.Authenticate(ProcessAuthentication);
+    }
+
+    internal void ProcessAuthentication(SignInStatus status)
+    {
+        if (status == SignInStatus.Success)
+        {
+            SceneOption.Instance.ChapterNum = PlayerPrefs.GetInt("UnlockedChapterNum");
+            SceneOption.Instance.UnitNum = PlayerPrefs.GetInt("UnlockedFinalUnitNum");
+            int licenseStatus = PlayerPrefs.GetInt("HasLicense");
+            if(licenseStatus == 1)
+            {
+                licenseUnlocked = true;
+            }
+            else
+            {
+                licenseUnlocked = false;
+            }
+            Debug.Log("Android login success");
+        }
+        else
+        {
+            // Disable your integration with Play Games Services or show a login button
+            // to ask users to sign-in. Clicking it should call
+            // PlayGamesPlatform.Instance.ManuallyAuthenticate(ProcessAuthentication).
+            Debug.Log("Android login failed");
+        }
+    }
+
     public void OnUnlockChapter()
     {
+        SignIn();
         licenseUnlocked = true;
+        PlayerPrefs.SetInt("HasLicense", 1);
     }
 
     public void OnUnlockFailed()
